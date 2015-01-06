9ab3d152e0eff06915197d2c7702e312bf8ff695
KingsleyZ <kingsleyzheng0716@gmail.com>
2015-01-06 14:39:41 -0700

    Tried to integrate dependency injection.

--- GreenReceiptWebApi.BLL/UserServices.cs
+++ GreenReceiptWebApi.BLL/UserServices.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GreenReceiptWebApi.BLL.Contracts;
+using GreenReceiptWebApi.BLL.Models;
+using GreenReceiptWebApi.DAL.RepositoryInterface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,105 @@ using System.Threading.Tasks;
 
 namespace GreenReceiptWebApi.BLL
 {
-    public class UserServices
+    public class UserServices : IUserServices
     {
+        /// <summary>
+        /// The user repository interface
+        /// </summary>
+        private readonly IUserRepository userRepository;
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        /// <param name="userRepository"></param>
+        public UserServices(IUserRepository userRepository)
+        {
+            if (userRepository == null)
+            {
+                throw new ArgumentNullException("userRepository");
+            }
+
+            this.userRepository = userRepository;
+        }
+
+        public User GetOrCreateUser(string claimedId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User CreateUser(User newUser)
+        {
+            if (newUser == null)
+            {
+                throw new ArgumentNullException("newUser");
+            }
+
+            try
+            {
+                GreenReceiptWebApi.DAL.Model.User userToAdd = ToDataModelUser(newUser);
+                this.userRepository.Create(userToAdd);
+                return ToServiceUser(userToAdd);
+            }
+            catch (InvalidOperationException ex)
+            {
+                //throw new BusinessServicesException(Resources.UnableToCreateUserExceptionMessage, ex);
+                throw new Exception("CreateUser");
+            }
+        }
+
+        public void UpdateUser(User updatedUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convert BLL.Model.User to DAL.Model.User
+        /// </summary>
+        /// <param name="userToConvert">DAL.Model.User</param>
+        /// <returns>Return BLL.Model.User</returns>
+        internal static GreenReceiptWebApi.DAL.Model.User ToDataModelUser(User userToConvert)
+        {
+            if (userToConvert == null)
+            {
+                return null;
+            }
+
+            GreenReceiptWebApi.DAL.Model.User modelUser = new GreenReceiptWebApi.DAL.Model.User()
+            {
+                UserId = userToConvert.UserId,
+                FirstName = userToConvert.FirstName,
+                LastName = userToConvert.LastName,
+                Email = userToConvert.Email,
+                Password = userToConvert.Password,
+                Username = userToConvert.Username,
+            };
+
+            return modelUser;
+        }
+
+        /// <summary>
+        /// Convert DAL.Model.User to BLL.Model.User
+        /// </summary>
+        /// <param name="dataModelUser">DAL.Model.User</param>
+        /// <returns>Return BLL.Model.User</returns>
+        internal static User ToServiceUser(GreenReceiptWebApi.DAL.Model.User dataModelUser)
+        {
+            if (dataModelUser == null)
+            {
+                return null;
+            }
+
+            User user = new User()
+            {
+                UserId = dataModelUser.UserId,
+                FirstName = dataModelUser.FirstName,
+                LastName = dataModelUser.LastName,
+                Email = dataModelUser.Email,
+                Password = dataModelUser.Password,
+                Username = dataModelUser.Username,
+            };
+
+            return user;
+        }
     }
 }
---
+++ GreenReceiptWebApi.Web/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using GreenReceiptWebApi.BLL.Contracts;
+using GreenReceiptWebApi.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GreenReceiptWebApi.Web.Controllers
+{
+    public class UserController : ApiController
+    {
+        private readonly IUserServices userServices;
+
+        public User Get()
+        {
+            return new User() { UserId = 1, Email = "test", FirstName = "f", LastName = "l", Password = "123", Username = "user" };
+        }
+
+
+    }
+}

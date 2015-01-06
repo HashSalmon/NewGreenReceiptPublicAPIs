9f2fa9142c6167464a94639754d45084b3d492d6
KingsleyZ <kingsleyzheng0716@gmail.com>
2015-01-06 16:07:19 -0700

    Added Unity framework for Dependency Injection.

---
+++ GreenReceiptWebApi.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using GreenReceiptWebApi.DAL.UnitOfWorkInterface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenReceiptWebApi.DAL.Repositories
+{
+    /// <summary>
+    /// This class represents a base repository, every repository will
+    /// extends from this base repository
+    /// </summary>
+    public class BaseRepository
+    {
+        protected IUnitOfWork UnitOfWork { get; set; }
+
+        protected GreenReceiptWebApiDbContext Context
+        {
+            get { return (GreenReceiptWebApiDbContext)this.UnitOfWork; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="unitOfWork">Unit of work</param>
+        public BaseRepository(IUnitOfWork unitOfWork)
+        {
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWork");
+            }
+
+            this.UnitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Get a generic Set from database
+        /// </summary>
+        /// <typeparam name="TEntity">Generic TEntity type</typeparam>
+        /// <returns>Return a Set of type TEntity from database</returns>
+        protected virtual DbSet<TEntity> GetDbSet<TEntity>() where
+            TEntity : class
+        {
+            return this.Context.Set<TEntity>();
+        }
+
+        /// <summary>
+        /// Set an entity state 
+        /// </summary>
+        /// <param name="entity">An entity</param>
+        /// <param name="entityState">The entity's state</param>
+        protected virtual void SetEntityState(object entity, EntityState entityState)
+        {
+            this.Context.Entry(entity).State = entityState;
+        }
+    }
+}
---
+++ GreenReceiptWebApi.DAL/UnitOfWorkInterface/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenReceiptWebApi.DAL.UnitOfWorkInterface
+{
+    /// <summary>
+    /// Encapsulates a unit of work
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Save changes to all objects that have changed within the unit of work
+        /// </summary>
+        void SaveChanges();
+    }
+}
